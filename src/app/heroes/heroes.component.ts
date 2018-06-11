cc8378067dfe69570ce107dd2ad35f3aaf30da90
Glen <glenrg@gmail.com>
2018-06-11 11:27:23 -0700

    Uses a seperate component to show Hero Details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,12 +8,17 @@ import { HEROES } from './mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero:Hero = {
-    id :1,
-    name:'Windstorm',
-  };
+  selectedHero:Hero;
+  // hero:Hero = {
+  //   id :1,
+  //   name:'Windstorm',
+  // };
 
   heroes = HEROES;
+
+  onSelect(hero:Hero):void{
+    this.selectedHero=hero;
+  }
   
   constructor() { }
 
